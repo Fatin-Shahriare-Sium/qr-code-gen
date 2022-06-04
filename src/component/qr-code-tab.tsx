526f9d0abe4f23972e1eefd8e96c5921c5e62a8c
fatin shahriare sium <sium1206@gmail.com>
2022-06-04 06:59:13 +0000

    alhamdulillah,doing coding after a week by the the grace of Allah,#allahisalmighty"

--- src/component/qr-code-tab.tsx
+++ src/component/qr-code-tab.tsx
@@ -1,20 +1,37 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Outlet } from 'react-router'
-
+import { Link, useLocation } from 'react-router-dom'
+import infoSvg from '../image/info.svg';
+import wifiSvg from '../image/wifi.svg'
+import textSvg from '../image/text.svg'
 function QrCodeTab() {
+    let location = useLocation()
+    useEffect(() => {
+        console.log(location);
+
+    }, [location])
     return (
         <div>
             <div className='qr-code-tab__wrapper'>
                 <div className='qr-code-tab__container'>
-                    <div className='qr-code-tab__single'>
-                        <p>Info</p>
-                    </div>
-                    <div className='qr-code-tab__single'>
-                        <p>Wifi</p>
-                    </div>
-                    <div className='qr-code-tab__single'>
-                        <p>Text</p>
-                    </div>
+                    <Link to='/gen-qr-code/info'>
+                        <div className={location.pathname !== "/gen-qr-code/info" ? "qr-code-tab__single" : "qr-code-tab__single tab-active"}>
+                            <img src={infoSvg} alt="" />
+                            <p>Info</p>
+                        </div>
+                    </Link>
+                    <Link to='/gen-qr-code/wifi'>
+                        <div className={location.pathname !== "/gen-qr-code/wifi" ? "qr-code-tab__single" : "qr-code-tab__single tab-active"}>
+                            <img src={wifiSvg} alt="" />
+                            <p>Wifi</p>
+                        </div>
+                    </Link>
+                    <Link to="/gen-qr-code/text">
+                        <div className={location.pathname !== "/gen-qr-code/text" ? "qr-code-tab__single" : "qr-code-tab__single tab-active"}>
+                            <img src={textSvg} alt="" />
+                            <p>Text</p>
+                        </div>
+                    </Link>
                 </div>
             </div>
             <div>
